e070d89cdb110aa2b70aaaa3d46a1583633f7262
rahulkumarhavit@gmail.com <allblogwork24@gmail.com>
2024-12-21 23:58:25 +0530

    shadcn ui

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <form action="">
+            <h1 className='font-bold text-2xl mb-4 text-yellow-400 text-center'>Sign Up</h1>
+        </form>
+    </div>
+  )
+}
+
+export default page
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Navbar() {
     return (
         <div className='text-white'>
-            <div>
+            <div className='flex items-center justify-between px-5 mx-auto max-w-7xl h-16'>
                 <div>
                     <Link href="/">
                         <h1 className='text-2xl font-bold'>
@@ -13,9 +13,34 @@ function Navbar() {
                     </Link>
                 </div>
                 <div>
-                    <ul>
-                        <li>
+                    <ul className='flex font-medium items-center gap-5'>
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
                             <Link href="/dashboard/companies">Companies</Link>
+                        </li>
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
+                            <Link href="/dashboard/jobs">Jobs</Link>
+                        </li>
+
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
+                            <Link href="/findjobs">Find Jobs</Link>
+                        </li>
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
+                            <Link href="/dashboard/favorite">Favorites</Link>
+                        </li>
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
+                            <Link href="/profile">Profile</Link>
+                        </li>
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
+                            Logout
+                        </li>
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
+                            <Link href="/login">Login</Link>
+                        </li>
+                        <li className='hover:text-yellow-400 duration-300 cursor-pointer'>
+                            <Link href="/register">Signup</Link>
                         </li>
                     </ul>
                 </div>
